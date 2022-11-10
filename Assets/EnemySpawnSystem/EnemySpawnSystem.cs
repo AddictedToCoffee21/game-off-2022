31930e77e6bbaf2704d00987cc415c417f72cef6
Daki <sebastian.geheeb@gmx.de>
2022-11-10 23:39:27 +0100

    further work

--- Assets/EnemySpawnSystem/EnemySpawnSystem.cs
+++ Assets/EnemySpawnSystem/EnemySpawnSystem.cs
@@ -1,23 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class EnemySpawnSystem : MonoBehaviour
 {
+    public enum GameState
+    {
+        InWave,
+        AfterWave
+    }
+
     public Camera playerCamera;
     public Rigidbody2D playerRigidbody2D;
     public GameObject fly;
-    
-    [Space(10)]
-    
-    public List<EnemyWave> enemyWaves;
+
+    [Space(10)] public List<EnemyWave> enemyWaves;
     public int maxEnemyCount;
+    public int timeBetweenEnemySpawn = 1;
 
 
     private List<Queue<GameObject>> _enemyWaveQueue;
-    
+
     private float _rand1;
     private float _rand2;
 
@@ -30,16 +36,19 @@ public class EnemySpawnSystem : MonoBehaviour
     private float _xOuterBorderRight;
     private float _yOuterBorderDown;
     private float _yOuterBorderUp;
-
-    private bool _spawnWave;
-
+    
     private int _currentWave;
+
+    private float _currentTime;
+    private int _maxTime;
+    private GameState _currentGameState;
+    private bool _canSpawnEnemy;
 
     private void Start()
     {
         Vector2 bottomLeft = playerCamera.ScreenToWorldPoint(new Vector3(0, 0, playerCamera.nearClipPlane));
         Vector2 topRight = playerCamera.ScreenToWorldPoint(new Vector3(playerCamera.pixelWidth, playerCamera.pixelHeight, playerCamera.nearClipPlane));
-        
+
         _xInnerBorderLeft = bottomLeft.x - 1;
         _xInnerBorderRight = topRight.x + 1;
         _yInnerBorderDown = bottomLeft.y - 1;
@@ -51,8 +60,11 @@ public class EnemySpawnSystem : MonoBehaviour
         _yOuterBorderUp = _yInnerBorderUp + 3;
 
         _enemyWaveQueue = new List<Queue<GameObject>>();
-        _spawnWave = true;
         _currentWave = 0;
+        _currentTime = 0;
+        _canSpawnEnemy = false;
+
+        _currentGameState = GameState.InWave;
 
         for (int i = 0; i < enemyWaves.Count; i++)
         {
@@ -62,18 +74,79 @@ public class EnemySpawnSystem : MonoBehaviour
 
     private void Update()
     {
-        if(_spawnWave)
+        Vector2 bottomLeft = playerCamera.ScreenToWorldPoint(new Vector3(0, 0, playerCamera.nearClipPlane));
+        Vector2 topRight = playerCamera.ScreenToWorldPoint(new Vector3(playerCamera.pixelWidth, playerCamera.pixelHeight, playerCamera.nearClipPlane));
+
+        _xInnerBorderLeft = bottomLeft.x - 1;
+        _xInnerBorderRight = topRight.x + 1;
+        _yInnerBorderDown = bottomLeft.y - 1;
+        _yInnerBorderUp = topRight.y + 1;
+
+        _xOuterBorderLeft = _xInnerBorderLeft - 3;
+        _xOuterBorderRight = _xInnerBorderRight + 3;
+        _yOuterBorderDown = _yInnerBorderDown - 3;
+        _yOuterBorderUp = _yInnerBorderUp + 3;
+        
+        
+        
+        if (transform.childCount < maxEnemyCount)
+            _currentTime += Time.deltaTime;
+
+        if (_currentTime >= timeBetweenEnemySpawn)
         {
-            StartCoroutine(SpawnEnemy());
-            _spawnWave = false;
+            _currentTime = 0;
+            _canSpawnEnemy = true;
         }
 
-        if (_enemyWaveQueue[_currentWave].Count == 0)
+        if (_enemyWaveQueue[_currentWave].Count == 0 && transform.childCount == 0)
         {
-            _currentWave++;
-            _spawnWave = true;
+            _currentGameState = GameState.AfterWave;
+        }
+
+        switch (_currentGameState)
+        {
+            case GameState.InWave:
+            {
+                if (_canSpawnEnemy && transform.childCount < maxEnemyCount && _enemyWaveQueue[_currentWave].Count != 0)
+                {
+                    while (_rand1 >= _xInnerBorderLeft && _rand1 <= _xInnerBorderRight &&
+                           _rand2 >= _yInnerBorderDown && _rand2 <= _yInnerBorderUp)
+                    {
+                        _rand1 = Random.Range(_xOuterBorderLeft, _xOuterBorderRight);
+                        _rand2 = Random.Range(_yOuterBorderDown, _yOuterBorderUp);
+                    }
+
+                    Instantiate(_enemyWaveQueue[_currentWave].Dequeue(), new Vector3(_rand1, _rand2, 1),
+                        Quaternion.identity, transform).GetComponent<Enemy>().target = playerRigidbody2D;
+
+                    _rand1 = 0;
+                    _rand2 = 0;
+                    _canSpawnEnemy = false;
+                }
+
+                break;
+            }
+
+            case GameState.AfterWave:
+            {
+                Debug.Log("over");
+                break;
+            }
         }
         
+        // if (_enemyWaveQueue[_currentWave].Count == 0)
+        // {
+        //     _currentWave++;
+        //     _spawnWave = true;
+        // }
+
+
+        // if(_spawnWave)
+        // {
+        //     StartCoroutine(SpawnEnemy());
+        //     _spawnWave = false;
+        // }
+        //
         //     var bottomLeft = (Vector2)playerCamera.ScreenToWorldPoint(new Vector3(0, 0, playerCamera.nearClipPlane));
         //     var topLeft = (Vector2)playerCamera.ScreenToWorldPoint(new Vector3(0, playerCamera.pixelHeight, playerCamera.nearClipPlane));
         //     var topRight = (Vector2)playerCamera.ScreenToWorldPoint(new Vector3(playerCamera.pixelWidth, playerCamera.pixelHeight, playerCamera.nearClipPlane));
@@ -86,26 +159,5 @@ public class EnemySpawnSystem : MonoBehaviour
         //     Debug.DrawLine(bottomLeft - offset1, bottomRight + offset2);
         //     Debug.DrawLine(topRight + offset1, topLeft - offset2);
         //     Debug.DrawLine(topRight + offset1, bottomRight + offset2);
-    }
-
-    private IEnumerator SpawnEnemy()
-    {
-        while(transform.childCount < maxEnemyCount)
-        {
-            if(_enemyWaveQueue[_currentWave].Count != 0)
-            {
-                while (_rand1 >= _xInnerBorderLeft && _rand1 <= _xInnerBorderRight && _rand2 >= _yInnerBorderDown && _rand2 <= _yInnerBorderUp)
-                {
-                    _rand1 = Random.Range(_xOuterBorderLeft, _xOuterBorderRight);
-                    _rand2 = Random.Range(_yOuterBorderDown, _yOuterBorderUp);
-                }
-                Instantiate(_enemyWaveQueue[_currentWave].Dequeue(), new Vector3(_rand1, _rand2, 1), Quaternion.identity, transform).GetComponent<Enemy>().target = playerRigidbody2D;
-
-                _rand1 = 0;
-                _rand2 = 0;
-
-                yield return new WaitForSeconds(1);
-            }
-        }
     }
 }
