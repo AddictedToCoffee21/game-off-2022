f894e7e69a01d4ea9c9bcf13aedf8de09edd8986
Franz Haagen <franzhaagen@web.de>
2022-11-21 17:22:12 +0100

    added Camera Follow do not touch it works now

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Range(0,1)]
+    public float _smoothTime = 0.125f;
+
+    public Rigidbody2D target;
+
+    private Vector3 _offset = new Vector3(0,0,-10);
+    private Vector3 _velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = (Vector3) target.position + _offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref _velocity, _smoothTime, float.PositiveInfinity, Time.deltaTime);
+        this.transform.position = smoothedPosition;
+    }
+
+}
